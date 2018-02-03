9e39fb4143705bdbc7b19c023bd30546c06aa612
IsaacGuan <yanranguan1994@gmail.com>
2018-02-02 22:22:20 -0500

    merge the four .csv files and convert it to a standard version

---
+++ convert.r
@@ -0,0 +1,3 @@
+setwd("d:/data mining and concept learning/Assignment 1/NYC-Parking-Tickets-Analysis")
+nyc_data = read.csv("Parking_Violations_Issued_sampled.csv", head = TRUE, sep = ",", quote = "\"")
+write.csv(nyc_data, "Parking_Violations_Issued_sampled_new.csv")
