23df58c628fd62322a0c66bb6c14d6996470838b
IsaacGuan <yanranguan1994@gmail.com>
2018-02-19 17:44:05 -0500

    choose 5 attributes as predictors for J48 decision tree

--- j48_vehicle_color.r
+++ j48_vehicle_color.r
@@ -9,5 +9,9 @@ nyc_data_cleaned <- data.frame(nyc_data_cleaned_matrix)
 nyc_data_cleaned_factored <- lapply(nyc_data_cleaned, as.factor)
 target <- nyc_data_cleaned_factored[["Vehicle.Color"]]
 plate_type <- nyc_data_cleaned_factored[["Plate.Type"]]
-result_J48 <- J48(target~., nyc_data_cleaned_factored)
+vehicle_body_type <- nyc_data_cleaned_factored[["Vehicle.Body.Type"]]
+vehicle_make <- nyc_data_cleaned_factored[["Vehicle.Make"]]
+vehicle_year <- nyc_data_cleaned_factored[["Vehicle.Year"]]
+registration_state <- nyc_data_cleaned_factored[["Registration.State"]]
+result_J48 <- J48(target ~ plate_type + vehicle_body_type + vehicle_make + vehicle_year + registration_state, nyc_data_cleaned_factored)
 summary(result_J48)
